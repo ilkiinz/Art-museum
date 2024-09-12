a6128811e79fec6f09e3ea506e973fa958d653f9
ilkiinz <aqayev2205@gmail.com>
2024-09-12 19:13:19 +0400

    3 img section folder

---
+++ main.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+        setTimeout(function() {
+            document.querySelector("header button").style.animation = "moveButton 1.5s forwards";
+        }, 1000); 
+    };
