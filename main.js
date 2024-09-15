754aeaf21648128ebcb343fc72eb6636fa576f66
ilkiinz <aqayev2205@gmail.com>
2024-09-16 02:40:49 +0400

    add responsive section and add new section

--- main.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-        setTimeout(function() {
-            document.querySelector("header button").style.animation = "moveButton 1.5s forwards";
-        }, 1000); 
-    };
